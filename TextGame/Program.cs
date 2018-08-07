3a0d49249352dfd0dd49ed6dcf691f6e85ba2395
Alex Blundell <8362291+Alex-Blundell@users.noreply.github.com>
2018-08-07 13:25:51 +1200

    Add files via upload

---
+++ TextGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TextGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Health;
+            int Mana;
+            string uName;
+            int MenuIndex = 0;
+            int Index = 0;
+
+            while (Index == 0)
+            {
+                Console.WriteLine("\n1. Start New Game");
+                Console.WriteLine("2. Load Game");
+                Console.WriteLine("3. Exit\n");
+                Console.Write(":");
+
+                MenuIndex = Convert.ToInt32(Console.ReadLine());
+
+                if (MenuIndex == 1)
+                {
+                    Index = 1;
+                }
+                else if (MenuIndex == 2)
+                {
+                    Index = 2;
+                }
+                else if (MenuIndex == 3)
+                {
+                    // Close Application
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
